82556b2af56d391404b3536bc45efbc6b4efa99d
Allan Cleysson <allancleysson@gmail.com>
2017-09-21 10:06:45 -0300

    Added Portuguese language and support for Brazil as Country.

--- Source/PartnerCenter.CustomerPortal/Resources.Designer.cs
+++ Source/PartnerCenter.CustomerPortal/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Store.PartnerCenter.CustomerPortal {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -651,6 +651,15 @@ namespace Microsoft.Store.PartnerCenter.CustomerPortal {
         public static string CountryNameBG {
             get {
                 return ResourceManager.GetString("CountryNameBG", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Brazil.
+        /// </summary>
+        public static string CountryNameBR {
+            get {
+                return ResourceManager.GetString("CountryNameBR", resourceCulture);
             }
         }
         
