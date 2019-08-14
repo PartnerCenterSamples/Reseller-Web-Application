a600a0788e114a2ff05189f7b706d9d12f648e38
Isaiah Williams <isaiah.williams@microsoft.com>
2019-08-14 10:34:09 -0500

    Adding one of the new supported countries

--- src/Storefront/Resources.Designer.cs
+++ src/Storefront/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Microsoft.Store.PartnerCenter.Storefront
-{
-
-
+namespace Microsoft.Store.PartnerCenter.Storefront {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,7 +19,7 @@ namespace Microsoft.Store.PartnerCenter.Storefront
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -642,6 +642,15 @@ namespace Microsoft.Store.PartnerCenter.Storefront
         public static string CouldNotRetrieveSubscriptionsSummary {
             get {
                 return ResourceManager.GetString("CouldNotRetrieveSubscriptionsSummary", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Angola.
+        /// </summary>
+        public static string CountryNameAO {
+            get {
+                return ResourceManager.GetString("CountryNameAO", resourceCulture);
             }
         }
         
